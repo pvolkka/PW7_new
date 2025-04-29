8ab4893dee8ecb4fad93f7d6d41cab5243d0ca3a
pvolkka <ssvit2000@gmail.com>
2025-04-29 23:08:25 +0300

    Лабораторна робота №7. ООП. Створення об'єктів класу (мова Java). Створила 4 об'єкти класу Customer.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String[] args) {
+        Customer customer1 = new Customer("Тарас", (byte) 19, 274, 15);
+        Customer customer2 = new Customer("Олександр", (byte) 21, 725, 74);
+        Customer customer3 = new Customer("Вероніка", (byte) 17, 56, 10);
+        Customer customer4 = new Customer("Даніїл", (byte) 18, 836, 34);
+    }
 }
